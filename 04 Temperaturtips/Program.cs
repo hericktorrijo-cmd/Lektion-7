523e4857dc13823365904e1091717894e8b7d088
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-23 17:04:55 +0200

    uppgift 4 klar

---
+++ 04 Temperaturtips/Program.cs
@@ -0,0 +1,16 @@
+﻿GeKlädråd(10);
+
+Console.WriteLine("\n\nTryck på valfri knapp för att stänga konsolen...");
+Console.ReadKey();
+
+static void GeKlädråd(double temperatur)
+{
+    if (temperatur < 10)
+    {
+        Console.WriteLine("Ta på dig en tjock jacka!");
+    }
+    else
+    {
+        Console.WriteLine("En tröja räcker gott!");
+    }
+}
