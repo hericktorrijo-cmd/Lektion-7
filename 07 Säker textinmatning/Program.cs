95933fccfe6db050e6c821abaebc39e4fc1adeed
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-23 18:30:31 +0200

    uppgift 7 klar

---
+++ 07 Säker textinmatning/Program.cs
@@ -0,0 +1,26 @@
+﻿string namn = LäsSäkerText("Vad är ditt namn: ");
+Console.WriteLine($"Hej {namn}, ha en trevlig eftermiddag!");
+
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
+
+
+static string LäsSäkerText(string ledText)
+{
+    while(true)
+    {
+        Console.Write(ledText);
+        string text = Console.ReadLine();
+
+        if(!string.IsNullOrWhiteSpace(text))
+        {
+            return text;
+        }
+
+        else
+        {
+            Console.WriteLine("Felaktig inmatning, försök igen.");
+        }
+    }
+}
