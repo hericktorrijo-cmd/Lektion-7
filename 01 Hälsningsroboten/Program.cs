eee4a9812e9edd97d9c7446534da211ef7678954
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-23 16:49:23 +0200

    Add project files.

---
+++ 01 Hälsningsroboten/Program.cs
@@ -0,0 +1,11 @@
+﻿VisaHälsning();
+
+Console.WriteLine("\n\nTryck på valfri knapp för att stänga konsolen...");
+Console.ReadKey();
+
+static void VisaHälsning()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Välkomen till systemet! Robot aktvierad.");
+}
