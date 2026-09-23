e4115ceab11f0e8029cfe5de5093e2f804f11ceb
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-23 17:00:04 +0200

    uppgift 3 klar

---
+++ 03 Personlig hälsning/Program.cs
@@ -0,0 +1,9 @@
+﻿HälsaAnvändare("Herick");
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
+
+static void HälsaAnvändare(string name)
+{
+    Console.WriteLine($"Hej {name}, hoppas du har en fantastisk dag på distansutbilningen!");
+}
