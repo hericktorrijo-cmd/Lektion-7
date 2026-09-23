e601e8c7c6b399f907594c1331ecc07d2355f411
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-23 16:57:27 +0200

    uppgift 2 klar

---
+++ 02 SkrivLinje/Program.cs
@@ -0,0 +1,17 @@
+﻿SkrivAvskiljare();
+Console.WriteLine("Skriver lite text!\n");
+SkrivAvskiljare();
+Console.WriteLine("Skriver lite mer text!\n");
+SkrivAvskiljare();
+Console.WriteLine("Skriver sista text!");
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
+static void SkrivAvskiljare()
+{
+    for(int i = 0; i < 30; i++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
