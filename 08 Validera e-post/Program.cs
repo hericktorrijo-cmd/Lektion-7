78b73ed443765b0b1b959482be3783248a2394a6
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-24 20:33:00 +0200

    uppgift 8 klar

---
+++ 08 Validera e-post/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write("Ange din epost: ");
+string epost = Console.ReadLine();
+
+Console.WriteLine(ÄrGiltigEpost(epost));
+
+Console.WriteLine("\n\nTryck på valfri knapp för att stänga konsolen...");
+Console.ReadKey();
+
+
+
+
+
+
+
+static bool ÄrGiltigEpost(string epost)
+{
+    return epost.Contains("@") &&
+    epost.Contains(".") &&
+    epost.Length >= 5;
+
+}
