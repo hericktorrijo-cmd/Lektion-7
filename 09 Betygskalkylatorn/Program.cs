cadf1b1db84f7ae426fbab4086ffae6e634a5db7
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-24 20:46:10 +0200

    uppgift 9 klar

---
+++ 09 Betygskalkylatorn/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.Write("Hur mycket poäng fick du: ");
+if(int.TryParse(Console.ReadLine(), out int poäng))
+{
+    Console.WriteLine(FastställBetyg(poäng));
+}
+else
+{
+    Console.WriteLine("Felaktig inmatning");
+}
+
+
+Console.WriteLine("\n\nTryck på valfri knapp för att stänga konsolen...");
+Console.ReadKey();
+
+
+
+static string FastställBetyg(int poäng)
+{
+    if (poäng < 50)
+    return "IG";
+
+    if (poäng < 80)
+        return "G";
+
+    
+        return "VG";
+
+}
