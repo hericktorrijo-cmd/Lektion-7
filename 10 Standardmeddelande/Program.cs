df0d2ed3b5d846daae578447ba178aee3939cce4
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-24 21:08:58 +0200

    Uppgift 10 klar

---
+++ 10 Standardmeddelande/Program.cs
@@ -0,0 +1,12 @@
+﻿LoggaMeddelande("Jag heter HERICK!", "NAMN");
+LoggaMeddelande("Jag heter HERICK!");
+
+Console.WriteLine("\n\nTryck på valfri tangent för att stänga konsolen...");
+Console.ReadKey();
+
+
+
+static void LoggaMeddelande(string meddelande, string loggTyp = "INFO")
+{
+    Console.WriteLine($"[{loggTyp}] - {meddelande}"); 
+}
